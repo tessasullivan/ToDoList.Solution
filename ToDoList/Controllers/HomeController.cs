74768d7e476ffbafb19a9341c4e339b610c78d4c
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-04-30 09:16:01 -0700

    Created ItemsController.cs and moved all Item related routes to this file, created an Items View

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -10,19 +10,7 @@ namespace ToDoList.Controllers
     [HttpGet("/")]
     public ActionResult Index()
     {
-        List<Item> allItems = Item.GetAll();
-        return View(allItems);
-    }
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
-    {
         return View();
-    }
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-        Item myItem = new Item(description);
-        return RedirectToAction("Index");
     }
   }
 }
