c901743a5f8f10c8be11a9da57ee77767efde978
Tessa Sullivan <tessa.sullivan@gmail.com>
2019-04-30 15:49:09 -0700

    Added HomeControllerTests and ItemsControllerTests which pass

--- ToDoList.Tests/ControllerTests/HomeControllerTests.cs
+++ ToDoList.Tests/ControllerTests/HomeControllerTests.cs
@@ -1,47 +1,23 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ToDoList.Controllers;
 using ToDoList.Models;
 
-namespace ToDoList.Tests
-{
+namespace ToDoList.Tests {
     [TestClass]
-    public class HomeControllerTest
-    {
-        [TestMethod]
-        public void Index_ReturnsCorrectView_True()
-        {
-            HomeController controller = new HomeController();
-            ActionResult indexView = controller.Index();
-            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
-        }
+    public class HomeControllerTest {
 
         [TestMethod]
-        public void Index_HasCorrectModelType_ItemList()
-        {
-            // HomeController controller = new HomeController();
-            // ViewResult indexView = controller.Index() as ViewResult;
-            ViewResult indexView = new HomeController().Index() as ViewResult;
-            var result = indexView.ViewData.Model;
-            Assert.IsInstanceOfType(result, typeof(List<item>));
-        }
+        public void Index_ReturnsCorrectView_True () {
+            //Arrange
+            HomeController controller = new HomeController ();
 
-        [TestMethod]
-        public void Create_ReturnsCorrectActionType_RedirectToActionResult()
-        {
-            ItemsController controller = new ItemsController();
-            IActionResult view = controller.Create("Walk the dog");
-            Assert.IsInstanceOfType(view, typeof(RedirectToActionResult));
-        }
+            //Act
+            ActionResult indexView = controller.Index ();
 
-        [TestMethod]
-        public void Create_RedirectsToCorrectAction_Index()
-        {
-            ItemsController controller = new ItemsController();
-            RedirectToActionResult actionResult = controller.Create("Walk the dog") as RedirectToActionResult;
-            string result = actionResult.ActionName;
-            Assert.AreEqual(result, "Index");
+            //Assert
+            Assert.IsInstanceOfType (indexView, typeof (ViewResult));
         }
     }
 }
