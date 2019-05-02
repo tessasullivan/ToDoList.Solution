d3cc5771f5fd2c6e2a7d303b19b5870b2f0196d5
Student <example@example.com>
2019-05-02 08:22:59 -0700

    Updated CategoriesController

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -13,18 +13,30 @@ namespace ToDoList.Controllers
         List<Category> allCategories = Category.GetAll();
         return View(allCategories);
     }
-
+    [HttpPost("/categories")]
+    public ActionResult Create(string categoryName)
+    {
+      Category newCategory = new Category(categoryName);
+      List<Category> allCategories = Category.GetAll();
+      return View("Index", allCategories);
+    }
     [HttpGet("/categories/new")]
     public ActionResult New()
     {
         return View();
     }
-    [HttpPost("/categories")]
-    public ActionResult Create(string categoryName)
+    // This one creates new Items within a given Category, not new Categories:
+    [HttpPost("/categories/{categoryId}/items")]
+    public ActionResult Create(int categoryId, string itemDescription)
     {
-        Category newCategory = new Category(categoryName);
-        List<Category> allCategories = Category.GetAll();
-        return View("Index", allCategories);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category foundCategory = Category.Find(categoryId);
+      Item newItem = new Item(itemDescription);
+      foundCategory.AddItem(newItem);
+      List<Item> categoryItems = foundCategory.GetItems();
+      model.Add("items", categoryItems);
+      model.Add("category", foundCategory); 
+      return View("Show", model);
     }
     [HttpGet("/categories/{id}")]
     public ActionResult Show(int id)
--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -6,35 +6,28 @@ namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
+    {
+       Category category = Category.Find(categoryId);
+       return View(category);
+    }
 
-    [HttpGet("/items")]
-    public ActionResult Index()
-    {
-        List<Item> allItems = Item.GetAll();
-        return View(allItems);
-    }
-    [HttpGet("/items/new")]
-    public ActionResult New()
-    {
-        return View();
-    }
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-        Item myItem = new Item(description);
-        return RedirectToAction("Index");
-    }
     [HttpPost("/items/delete")]
     public ActionResult DeleteAll()
     {
         Item.ClearAll();
         return View();
     }
-    [HttpGet("/items/{id}")]
-    public ActionResult Show(int id)
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
     {
-        Item item = Item.Find(id);
-        return View(item);
+        Item item = Item.Find(itemId);
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Category category = Category.Find(categoryId);
+        model.Add("item", item);
+        model.Add("category", category);
+        return View(model);
     }
   }
 }
