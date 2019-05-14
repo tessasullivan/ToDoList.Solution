c95996b500bd2e91184c49141abc04479132e70a
Student <example@example.com>
2019-05-14 15:25:59 -0700

    Reverted to previous GetCategories without join statement and included correct sql exported files

--- to_do_list_with_dueDate.sql
+++ to_do_list_with_dueDate.sql
@@ -3,18 +3,24 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
-
--- Generation Time: May 14, 2019 at 04:14 PM
-
+-- Generation Time: May 14, 2019 at 10:22 PM
 -- Server version: 5.6.38
 -- PHP Version: 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `to_do_list_with_dueDate`
 --
+CREATE DATABASE IF NOT EXISTS `to_do_list_with_dueDate` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `to_do_list_with_dueDate`;
 
 -- --------------------------------------------------------
 
@@ -32,7 +38,11 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`id`, `name`) VALUES
-(1, 'Housework');
+(1, 'Housework'),
+(2, 'moo'),
+(3, 'moo'),
+(4, 'moo'),
+(5, 'moo');
 
 -- --------------------------------------------------------
 
@@ -46,11 +56,12 @@ CREATE TABLE `categories_items` (
   `item_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
 --
 -- Dumping data for table `categories_items`
 --
 
+INSERT INTO `categories_items` (`id`, `category_id`, `item_id`) VALUES
+(2, 1, 10);
 
 -- --------------------------------------------------------
 
@@ -67,6 +78,10 @@ CREATE TABLE `items` (
 --
 -- Dumping data for table `items`
 --
+
+INSERT INTO `items` (`id`, `description`, `dueDate`) VALUES
+(9, 'vacuum', '2019-05-08'),
+(10, 'do the dishes', '2019-05-03');
 
 --
 -- Indexes for dumped tables
@@ -99,19 +114,20 @@ ALTER TABLE `items`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `categories_items`
 --
 ALTER TABLE `categories_items`
-
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- to_do_list_with_dueDate_test.sql
+++ to_do_list_with_dueDate_test.sql
@@ -3,9 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
-
--- Generation Time: May 14, 2019 at 04:14 PM
-
+-- Generation Time: May 14, 2019 at 10:23 PM
 -- Server version: 5.6.38
 -- PHP Version: 7.2.1
 
@@ -13,21 +11,46 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 --
 -- Database: `to_do_list_with_dueDate_test`
 --
+CREATE DATABASE IF NOT EXISTS `to_do_list_with_dueDate_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `to_do_list_with_dueDate_test`;
 
 -- --------------------------------------------------------
 
 --
--- Database: `to_do_list_with_dueDate_test`
+-- Table structure for table `categories`
 --
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`) VALUES
+(1, 'Home stuff'),
+(2, 'Home stuff'),
+(3, 'Home stuff'),
+(4, 'Home stuff'),
+(5, 'Home stuff'),
+(6, 'Work stuff'),
+(7, 'Home stuff'),
+(8, 'Home stuff');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `categories_items`
-
 --
 
 CREATE TABLE `categories_items` (
@@ -35,6 +58,17 @@ CREATE TABLE `categories_items` (
   `category_id` int(11) DEFAULT NULL,
   `item_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categories_items`
+--
+
+INSERT INTO `categories_items` (`id`, `category_id`, `item_id`) VALUES
+(1, 2, 9),
+(2, 4, 18),
+(3, 5, 26),
+(4, 7, 27),
+(5, 8, 35);
 
 -- --------------------------------------------------------
 
@@ -53,7 +87,6 @@ CREATE TABLE `items` (
 --
 
 --
-
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
@@ -68,31 +101,32 @@ ALTER TABLE `categories_items`
 
 --
 -- Indexes for table `items`
-
 --
-ALTER TABLE `categories_items`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
-
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `categories_items`
 --
 ALTER TABLE `categories_items`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
-ALTER TABLE `categories_items`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `items`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
